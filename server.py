5d98777a136b1b955b081b1e5dbe8eb1ffc9ee26
Tom Jacobs <tjdjacobs@gmail.com>
2022-04-25 21:29:13 +0200

    - Made server interactive with the possibility to set names, countdown and orange light\n- Made the server shutdown instead of crashing when trying to start when the port is still used\n- Added an exit function which does not always work

--- server.py
+++ server.py
@@ -15,6 +15,9 @@ class Server:
         self._startClientId = None
         self._finishClientId = None
         self._racers = [Racer("P1", 1, 1, 1), Racer("P2", 2, 2, 2)]
+        self._answer = ""
+        self._countdown = 3
+        self._orangeLightAt = 1
 
     def bothClientsConnected(self):
         return (self._startClientConnected and self._finishClientConnected)
@@ -64,7 +67,7 @@ class Server:
                 self.processEndTime(data, self._racers[1])
 
     def multi_threaded_client(self, connection, id):
-        while True:
+        while not self._answer == "exit":
             try:
                 data = connection.recv(1024).decode()
                 if not (self.bothClientsConnected()):
@@ -82,15 +85,14 @@ class Server:
                 break
         connection.close()
 
-    def countdown(self, t):
-        orangeLightAt = t / 3
+    def startCountdown(self, t):
         while t:
             mins, secs = divmod(t, 60)
             timer = '{:02d}:{:02d}'.format(mins, secs)
             print(timer, end="\r")
             time.sleep(1)
             t -= 1
-            if (t == orangeLightAt):
+            if (t == self._orangeLightAt):
                 for racer in self._racers:
                     if not (racer.getFalseStart()):
                         racer._light.turnOn(Color.Orange)
@@ -103,46 +105,115 @@ class Server:
         # get the hostname
         host = socket.gethostname()
         port = 5000  # initiate port no above 1024
-
         server_socket = socket.socket()  # get instance
-        # look closely. The bind() function takes tuple as argument
-        server_socket.bind((host, port))  # bind host address and port together
+        
+        # try to execute bind function, if it fails shut down the program
+        try:
+            server_socket.bind((host, port))  # bind host address and port together
+        except:
+            print("Poort is nog bezet, wacht 1 a 2 minuten")
+            self._answer = "exit"
 
         # configure how many clients the server can listen simultaneously
         server_socket.listen(2)
-        while True:
+        while not self._answer == "exit":
             if not self.bothClientsConnected():
                 self.repairConnection(server_socket)
 
+    def waitForAnswer(self):
+        while not self._answer == "exit":
+            time.sleep(0.1)
+            self._answer = input()
+
+    def waitForClients(self):
+        # added an extra sleep because of port is still busy and enter is pushed twice fast this loop would start and program would be stuck
+        time.sleep(0.1)
+        while (not self.bothClientsConnected()) and (not self._answer == "exit"):
+            print("Wacht op connectie met beide clients")
+            time.sleep(2)
+
+    def startRace(self):
+        self._status = State.WaitingForCountDown
+        self.waitForClients()
+        for racer in self._racers:
+            racer.reset()
+        print("\nNieuwe race gestart! Countdown is begonnen!")
+        self._status = State.CountingDown
+        self.startCountdown(self._countdown)
+        if (not self._status == State.RaceFinished):
+            self._status = State.RaceStarted
+            startTime = time.time()
+            print('GO!  ')
+            for racer in self._racers:
+                racer.startRace()
+                racer.setStartTime(startTime)
+            print("Starttijd: " + datetime.datetime.fromtimestamp(time.time()).ctime())
+
+        while (not self._status == State.RaceFinished) and (not self._answer == "stop") and (not self._answer == 'exit'):    
+            time.sleep(1)
+        print("Race gefinished, type een commando...")
+
+    def setNames(self):
+        try:
+            self._racers[0].setName(input("Geef naam van P1...\n"))
+            print("{} Raced als P1".format(self._racers[0].getName()))
+        except:
+            print("Ongeldige naam, instelling niet opgeslagen")
+        try:
+            self._racers[1].setName(input("Geef naam van P2...\n"))
+            print("{} Raced als P2".format(self._racers[1].getName()))
+        except:
+            print("Ongeldige naam, instelling niet opgeslagen")
+
+    def setCountdown(self):
+        try:
+            self._countdown = int(input("Geef aantal seconden...\n"))
+            print("countdown ingesteld op {} seconden".format(self._countdown))
+        except:
+            print("Ongeldig getal, instelling niet opgeslagen ")
+
+    def setOrangeLightAt(self):
+        try:
+            answer = int(input("Geef aantal seconden...\n"))
+            if answer <= self._countdown:
+                self._orangeLightAt = answer
+                print("Oranje licht ingesteld op {} seconden".format(self._orangeLightAt))
+            else:
+                print("Oranje licht sec mag niet hoger zijn dan countdown, instelling niet opgeslagen")
+        except:
+            print("Ongeldig getal, instelling niet opgeslagen")
+
+    def processCommand(self):
+        if self._answer == "start":
+            self.startRace()
+        elif self._answer == "namen":
+            self.setNames()
+        elif self._answer == "countdown":
+            self.setCountdown()
+        elif self._answer == "oranje":
+            self.setOrangeLightAt()
+        elif self._answer == "help":
+            self.printHelp()
+        elif self._answer == "exit":
+            print("poging tot programma afsluiten, mocht dit niet werken dan programma sluiten met Ctrl+c")
+        else:
+            print("type een geldig commando of type 'help'")
+        self._answer = ""
+
+    def printHelp(self):
+        print("Mogelijke commando's zijn: \n-'start' Om de countdown te beginnen.\n-'namen' Om namen van Racers in te geven.\n-'countdown' Om aantal sec countdown in te stellen.\n-'oranje' Om aantal sec vóór einde countdown in te stellen waarbij het oranje licht aangaat, default is 1.\n-'stop' Om de race af te breken, alleen te gebruiken tijdens de race.\n-'help' Om dit bericht te printen")
+
     def server_program(self):
         start_new_thread(self.keepSteadyConnection, ())
+        start_new_thread(self.waitForAnswer, ())
 
-        while True:
-            time.sleep(1)
-            while not self.bothClientsConnected():
-                print("Waiting for connection with clients")
-                time.sleep(2)
-            for racer in self._racers:
-                racer.reset()
-            print ("\nNieuwe race gestart!")
-            self._status = State.WaitingForCountDown
-            input("Drup op enter om de countdown te beginnen...\n")
-            self._status = State.CountingDown
-            self.countdown(3)
-            if (not self._status == State.RaceFinished):
-                self._status = State.RaceStarted
-                startTime = time.time()
-                print('GO!  ')
-                for racer in self._racers:
-                    racer.startRace()
-                    racer.setStartTime(startTime)
-                print("Starttijd: " + datetime.datetime.fromtimestamp(time.time()).ctime())
+        self.waitForClients()
+        if not self._answer == "exit":
+            self.printHelp()
 
-            while (not self._status == State.RaceFinished):
-                time.sleep(1)
-            input("Drup op enter om een nieuwe race te starten...\n")
-
-        conn.close()  # close the connection
+        while not self._answer == "exit":
+            if not self._answer == "":
+                self.processCommand()
 
 
 if __name__ == '__main__':
