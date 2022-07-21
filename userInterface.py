960c7b31a3e8ab2956752c7a65a9a6580a7fdbf0
Machoegel <Bartmachon@hotmail.com>
2022-07-21 22:05:58 +0200

    Split out Userinterface

---
+++ userInterface.py
@@ -0,0 +1,11 @@
+from raceSequence import RaceSequence
+from server import Server
+
+
+class UserInterface:
+    def __init__(self,server:Server,raceSequence:RaceSequence) -> None:
+        pass
+    def displayText(self,text:str):
+        pass
+    def start(self):
+        pass
