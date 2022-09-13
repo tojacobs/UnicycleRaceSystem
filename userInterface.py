9ae72bdab89c6dce99b376cbf3f91c06fa0a38a3
Tom Jacobs <tjdjacobs@gmail.com>
2022-09-13 17:08:17 +0200

    added end to the print function for the countdown and updated the help text

--- userInterface.py
+++ userInterface.py
@@ -5,7 +5,7 @@ from server import Server
 class UserInterface:
     def __init__(self,server:Server,raceSequence:RaceSequence) -> None:
         pass
-    def displayText(self,text:str):
+    def displayText(self, text:str, end:str):
         pass
     def start(self):
         pass
