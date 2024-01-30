da9e6a7693973332fb752134ac998d23fc32f71c
Machoegel <Bartmachon@hotmail.com>
2024-01-30 21:54:27 +0100

    Increases delay strartup start and finsh client manual test

--- tests/run_manual_test_windows.bat
+++ tests/run_manual_test_windows.bat
@@ -4,9 +4,9 @@ pip install -e .
 cd tests/
 copy client_config_finish.txt client_config.txt
 start call python ../raceSystem/client.py
-timeout 1
+timeout 2
 copy client_config_start.txt client_config.txt
 start call python ../raceSystem/client.py
 start call python ../raceSystem/UnicycleRaceSystem.py
-timeout 1
+timeout 2
 del client_config.txt
